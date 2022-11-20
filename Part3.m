3fff9166fd8cde1cc30020719c85b8f37e228e27
evan-sharafuddin <evansharafuddin@wustl.edu>
2022-11-20 13:27:42 -0600

    fixed error

--- Part3.m
+++ Part3.m
@@ -3,12 +3,17 @@ load lightField.mat
 % Set the distance you want to back propogate
 d = 1; % meters
 
-M_d = [1 -d 0 0 ;...
+% This distance was a "guess" but could also be determined using the
+% equation 1/f = 1/d_1 + 1/d_2, with the values for d_2 and f used in
+% part 2
+
+% This is the inverse of the propogation matrix M_d, derived in the report
+M_d_inverse = [1 -d 0 0 ;...
        0 1  0 0 ;...
        0 0  1 -d;...
        0 0  0 1 ];
 
-rays_final = M_d * rays;
+rays_final = M_d_inverse * rays;
 [rays_final_img, rays_final_x, rays_final_y] = ...
     rays2img(rays_final(1,:), rays_final(3,:), 0.005, 500);
 
